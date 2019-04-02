0dd1b248f30f40f882bdefa3190862f1037ea37f
Tobbe <tobbe@tripitaka.se>
2019-04-02 23:14:58 +1100

    update Morse

---
+++ agrid/accessories.py
@@ -0,0 +1,47 @@
+# Accessories 
+# Useful functions to build projects. 
+
+def testa_acc(test='HEJJA'):
+    A = np.ones(2,2)
+    print(test, A)
+    return 'apa'
+
+
+def download(url, filename, temp_path = None, check_exist=True):
+    '''
+    Class function to download data to default path.
+
+    '''
+
+    print('Im in download in sub')
+    # Check if already exist
+    if not check:
+        check = there_is_file
+
+    # Download if not
+
+    # To (named) temp
+
+    # Check if zip
+        #Uncompress
+
+    # Check if tar
+        # Uncompress
+    #
+
+    #Check that good
+
+
+
+
+
+
+#def open(file_name=None):
+#    '''
+#    Open dataset from netCDF. 
+#    file_name string
+#    returns dataset.
+#    '''
+#    if file_name == None:
+#        file_name = '%s.nc' % type(self).__name__
+#    return xr.open_dataset(file_name)
